49d3922f5b7b5f70037befe7d0059ce39dd81b2a
bernamartillano <bernamartillano@gmail.com>
2025-06-18 00:05:01 +0800

    put link

--- components/about.tsx
+++ components/about.tsx
@@ -30,12 +30,13 @@ export default function About() {
         . I’m also experienced with React and content management systems like{" "}
         <span className="font-medium underline"> WordPress </span> and{" "}
         <span className="font-medium underline"> Shopify </span>.
+        <br />
         <span>
-          See for more info/pictures of projects:
-          <span className="underline">
+          See for more info/pictures of projects: <br />
+          <a href="https://www.canva.com/design/DAGN1d2d1P0/5C0bOeOjjIPqW2gOHMlAug/view?utm_content=DAGN1d2d1P0&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h8ece5c4a41">
             {" "}
-            https://www.canva.com/design/DAGN1d2d1P0/5C0bOeOjjIPqW2gOHMlAug/view?utm_content=DAGN1d2d1P0&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h8ece5c4a41{" "}
-          </span>
+            Canva Projects{" "}
+          </a>
         </span>
       </p>
       <p className="mb-3">
